7adf1a767f10412dc276458d6358f1af968d735a
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-08-28 18:02:41 +0200

    added updated income/expense

--- src/components/expense/Expense.vue
+++ src/components/expense/Expense.vue
@@ -1,120 +1,122 @@
 <template>
-	<!-- @submit.prevent="submitExpense" -->
-	<form @submit.prevent="addExpense">
-		<span>Add a new expense</span>
-		<!--<label for="expense-category">Expense category</label>-->
-		<select v-model="v$.balanceChangeDTO.budgetCategoryID.$model" name="expense-category" id="expense-category">
-			<!-- Later when fetching -->
-			<!-- <option v-for="exCat in category" :key="exCat.id" value="">
-      {{ exCat.name }}
-    </option> -->
-			<option value="" disabled hidden>Select Category</option> <!-- This is coz our default value is an empty string so this will shot up by default.-->
-			<option value="1">Food</option>
-			<option value="2">Car</option>
-			<option value="3">Other</option>
-		</select>
-
-	
-
-		<input type="text" name="title" v-model="v$.balanceChangeDTO.title.$model" placeholder="Expense name:" />
-
-    <!-- v$ is so we use the validator component. same with $model.-->
-		<!-- this 2 rows are copy pasted from the validation @vuelidate/validators and will show error message if the validation rules are broken and will only show error if 
-    user has inputed something in the field aka it togles it to dirty first after user inputs.-->
-
-		<div class="input-errors" v-for="(error, index) of v$.balanceChangeDTO.title.$errors" :key="index">
-			<div class="error-msg">{{ error.$message }}</div>
-		</div>
-
-	
-		<input type="number" name="expense-amount" v-model="v$.balanceChangeDTO.amount.$model" placeholder="Amount:" /> 
-    <div class="input-errors" v-for="(error, index) of v$.balanceChangeDTO.amount.$errors" :key="index">
-			<div class="error-msg">{{ error.$message }}</div>
-		</div>
-     
-     <!-- old validation method  
-		<p v-if="balanceChangeDTO.amount === 0">{{ validationMsg }}</p>
+	<base-card>
+		<form @submit.prevent="submitExpense">
+			<div>
+				<label for="date">Expense category</label>
+				<select
+					name="expense-category"
+					id="expense-category"
+					v-model="state.balanceChangeDTO.budgetCategoryID"
+					@blur="v$.balanceChangeDTO.budgetCategoryID.$touch()"
+				>
+					<!-- Later when fetching -->
+					<!-- <option v-for="inCat in category" :key="inCat.id" value="">
+      {{ inCat.name }}
+    </option> 
+    !!!! Changed value to numbers as thats how the categeories are saved in the db.
+     @blur="v$.balanceChangeDTO.budgetCategoryID.$touch() is so that we validate after u highlight the field.
     -->
+					<option value="" hidden>Select income category</option>
+					<option value="1">Food</option>
+					<option value="2">Car</option>
+					<option value="3">Other</option>
+				</select>
 
-		<label for="balanceDate">Expense Date & Time</label>
-		<input type="date" name="balanceDate" v-model="v$.balanceChangeDTO.date.$model" />
-		<div class="input-errors" v-for="(error, index) of v$.balanceChangeDTO.date.$errors" :key="index">
-			<div class="error-msg">{{ error.$message }}</div>
-		</div>
+				<div v-if="this.v$.balanceChangeDTO.budgetCategoryID.$error">You need to pick a category.</div>
+			</div>
+      <input type="text" name="title" v-model="state.balanceChangeDTO.title" @blur="v$.balanceChangeDTO.title.$touch()" placeholder="Expense name:" />
+			<div>
+				<label for="income-amount">Balance:</label>
+				<input type="text" name="income-amount" v-model="state.balanceChangeDTO.amount" @blur="v$.balanceChangeDTO.amount.$touch()" />
+				<div class="input-errors" v-for="error of this.v$.balanceChangeDTO.amount.$error" :key="error.$uid">
+					<div class="error-msg">{{ error.$message }}</div>
+				</div>
+				<div v-if="this.v$.balanceChangeDTO.amount.$error">
+					{{ this.v$.balanceChangeDTO.amount.$errors[0].$message }}
+				</div>
+			</div>
 
-		<label for="description">Description</label>
-		<textarea
-			name="description"
-			id="description"
-			cols="30"
-			rows="3"
-			placeholder="Optional"
-			v-model.lazy="balanceChangeDTO.description"
-		></textarea>
-		<button :disabled="v$.balanceChangeDTO.$invalid" @click="addExpense">submit</button>
-		<p v-if="error !== ''">{{ error }} {{ resetRequestError }}</p>
-	</form>
+			<label for="date">Income Date & Time</label>
+			<input type="date" id="date" name="date" v-model="state.balanceChangeDTO.date" @blur="v$.balanceChangeDTO.date.$touch()" />
+			<div class="input-errors" v-for="error of v$.balanceChangeDTO.date.$error" :key="error.$uid">
+				<div class="error-msg">{{ error.$message }}</div>
+			</div>
+
+			<!-- <p v-if="date === ' '">{{ validationMsg }}</p> -->
+
+			<label for="description">Describe the expense details</label>
+			<textarea
+				name="description"
+				id="description"
+				cols="30"
+				rows="3"
+				placeholder="Optional"
+				v-model="state.balanceChangeDTO.description"
+			></textarea>
+			<button :disabled="this.v$.balanceChangeDTO.$invalid" @click="addIncome">submit</button>
+			<!-- <p v-if="error !== ''">{{ error }} {{ resetRequestError }}</p> -->
+		</form>
+	</base-card>
 </template>
 
 <script>
+import useValidate from '@vuelidate/core';
+import { required, numeric } from '@vuelidate/validators';
+import { reactive, computed } from 'vue';
 import axios from 'axios';
-import ExpenseNameField from './ExpenseNameField.vue';
-import useVuelidate from '@vuelidate/core';
-import { required, minLength } from '@vuelidate/validators';
 export default {
 	setup() {
-		return { v$: useVuelidate() }; //this on is to use the validator funktion
-	},
-	components: {
-		ExpenseNameField,
-	},
-	data() {
-		return {
-			balanceChangeDTO: { //now we can send balanceChangeDTO directly to endpoint as its formated same as the backend wants it.
+		const state = reactive({
+			balanceChangeDTO: {
 				budgetCategoryID: '',
-				title: '',
 				amount: '',
-				date: new Date().toJSON().slice(0, 10), //added todays date as the default date.
+        title: '',
+				date: new Date().toJSON().slice(0, 10),
 				description: '',
 			},
-			error: '',
-			validationMsg: 'input field required',
-		};
-	},
-	validations() {
+		});
+		const rules = computed(() => {
+			return {
+				balanceChangeDTO: {
+					budgetCategoryID: { required },
+					amount: { required, numeric },
+          title: {required},
+					date: { required },
+					description: {},
+				},
+			};
+		});
+		const v$ = useValidate(rules, state);
 		return {
-			balanceChangeDTO: {
-				// here we can add validation rules.
-				budgetCategoryID: { required }, // matches this.balanceChangeDTO.budgetCategoryID
-				title: { required }, // matches this.balanceChangeDTO.title
-				amount: { required },
-				date: { required },
-			},
+			state,
+			v$,
 		};
 	},
-
-	computed: {
-		resetRequestError() { //legazy code as validation is now used by an other function
-			setTimeout(() => {
-				this.error = '';
-			}, 3000);
-		},
+	data() {
+		return {};
 	},
 	methods: {
-		submitExpense() {
-			axios
-				.post('api', { // adam: have not done anything here realy. 
-					// keys must match DTO
-					budgetCategoryID: this.budgetCategoryID == '' ? this.validationMsg : this.budgetCategoryID,
-					amount: this.amount == 0 ? this.validationMsg : this.amount,
-					dateTime: this.dateTime == '' ? this.validationMsg : this.dateTime,
-					description: this.description == '' ? this.validationMsg : this.description,
-				})
-				.then((res) => console.log(res))
-				.catch((error) => (this.error = error.message));
+		submitIncome() {
+			// axios
+			//   .post("api", {
+			//     // keys must match DTO
+			//     budgetCategoryID:
+			//       this.budgetCategoryID == ""
+			//         ? this.validationMsg
+			//         : this.budgetCategoryID,
+			//     amount:
+			//       this.amount == 0 ? this.validationMsg : this.amount,
+			//     date: this.date == "" ? this.validationMsg : this.date,
+			//     description:
+			//       this.description == "" ? this.validationMsg : this.description,
+			//   })
+			//   .then((res) => console.log(res))
+			//   .catch((error) => (this.error = error.message));
+			this.v$.$validate();
 		},
-		addExpense() {
-			console.log(this.balanceChangeDTO);
+		addIncome() {
+			console.log(this.v$.balanceChangeDTO.amount.$error);
+			console.log(this.state.balanceChangeDTO);
 		},
 	},
 };
@@ -128,3 +130,5 @@ form {
 	padding: 0 10%;
 }
 </style>
+
+
--- src/components/income/Income.vue
+++ src/components/income/Income.vue
@@ -7,19 +7,24 @@
           name="income-category"
           id="income-category"
           v-model="state.incomeDTO.selectedIncomeCat"
+           @blur="v$.incomeDTO.selectedIncomeCat.$touch()"
         >
           <!-- Later when fetching -->
           <!-- <option v-for="inCat in category" :key="inCat.id" value="">
       {{ inCat.name }}
-    </option> -->
+    </option> 
+    !!!! Changed value to numbers as thats how the categeories are saved in the db.
+     @blur="v$.incomeDTO.selectedIncomeCat.$touch() is so that we validate after u highlight the field.
+    -->
           <option value="" hidden>Select income category</option>
-          <option value="salary">Salary</option>
-          <option value="loan">loan</option>
-          <option value="other">Other</option>
+          <option value="1">Salary</option>
+          <option value="2">loan</option>
+          <option value="3">Other</option>
         </select>
-        <p v-if="v$.incomeDTO.selectedIncomeCat.$error">
-          {{ v$.incomeDTO.selectedIncomeCat.$errors[0].$message }}
-        </p>
+
+        <div v-if="this.v$.incomeDTO.selectedIncomeCat.$error">
+        You need to pick a category.
+        </div>
       </div>
       <div>
         <label for="income-amount">Balance:</label>
@@ -27,10 +32,14 @@
           type="text"
           name="income-amount"
           v-model="state.incomeDTO.incomeAmount"
+          @blur="v$.incomeDTO.incomeAmount.$touch()"
         />
-        <p v-if="v$.incomeDTO.incomeAmount.$error">
-          {{ v$.incomeDTO.incomeAmount.$errors[0].$message }}
-        </p>
+         <div class="input-errors" v-for="error of this.v$.incomeDTO.incomeAmount.$error" :key="error.$uid">
+    <div class="error-msg">{{ error.$message }}</div>
+  </div>
+        <div v-if="this.v$.incomeDTO.incomeAmount.$error">
+          {{ this.v$.incomeDTO.incomeAmount.$errors[0].$message }}
+        </div>
       </div>
 
       <label for="date">Income Date & Time</label>
@@ -39,7 +48,12 @@
         id="date"
         name="date"
         v-model="state.incomeDTO.dateTime"
+        @blur="v$.incomeDTO.dateTime.$touch()"
       />
+       <div class="input-errors" v-for="error of v$.incomeDTO.dateTime.$error" :key="error.$uid">
+    <div class="error-msg">{{ error.$message }}</div>
+  </div>
+ 
       <!-- <p v-if="dateTime === ' '">{{ validationMsg }}</p> -->
 
       <label for="description">Describe the income details</label>
@@ -51,7 +65,7 @@
         placeholder="Optional"
         v-model="state.incomeDTO.description"
       ></textarea>
-      <button>submit</button>
+      <button :disabled="this.v$.incomeDTO.$invalid" @click="addIncome">submit</button>
       <!-- <p v-if="error !== ''">{{ error }} {{ resetRequestError }}</p> -->
     </form>
   </base-card>
@@ -72,19 +86,18 @@ export default {
         description: "",
       },
     });
-
+  
     const rules = computed(() => {
       return {
         incomeDTO: {
           selectedIncomeCat: { required },
           incomeAmount: { required, numeric },
           dateTime: { required },
+          description: {},
         },
       };
     });
-
     const v$ = useValidate(rules, state);
-
     return {
       state,
       v$,
@@ -93,7 +106,6 @@ export default {
   data() {
     return {};
   },
-
   methods: {
     submitIncome() {
       // axios
@@ -113,6 +125,10 @@ export default {
       //   .catch((error) => (this.error = error.message));
       this.v$.$validate();
     },
+   addIncome() {
+    console.log(this.v$.incomeDTO.incomeAmount.$error);
+    console.log(this.state.incomeDTO);
+   }
   },
 };
 </script>
