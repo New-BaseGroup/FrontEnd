64e33e1189c239bcac9a4eb523c0bc0891d7ac44
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-09-05 19:08:51 +0200

    fontawesome config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from '@/store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "@/store";
 import BaseCard from "./components/UI/BaseCard.vue";
+import "./assets/tailwind.css";
 
-const app = createApp(App);
-app.component("base-card", BaseCard);
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons"; //all reg icons
+import { fas } from "@fortawesome/free-solid-svg-icons"; //all solid icons
+import { fab } from "@fortawesome/free-brands-svg-icons"; //all barnds icons
 
-app.use(router)
-app.use(store)
+library.add(far, fas, fab);
+
+const app = createApp(App)
+  .component("base-card", BaseCard)
+  .component("font-awesome-icon", FontAwesomeIcon);
+app.use(router);
+app.use(store);
 
 app.mount("#app");
