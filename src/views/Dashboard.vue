dfc4009acdefe6f955c4ae0786f81bcfa86430bd
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-09-05 19:09:21 +0200

    dashboard template design

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center mt-16">
+    <div>
+      <font-awesome-icon
+        class="absolute text-xl text-[#ADAFAF] top-[70px] left-[730px]"
+        :icon="['fas', 'magnifying-glass']"
+      />
+      <input
+        @click="insertNewlineAndFocus"
+        type="text"
+        name="search"
+        id="search"
+        class="bg-[#F6F6F6] border-2 rounded-2xl h-8 indent-8 outline-0 focus:border-[#ADAFAF]"
+        size="60"
+        placeholder="Search"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
