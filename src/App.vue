515064db1a80e39edb29b16167f28212ed9dcc0c
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-09-19 16:36:47 +0200

    added start to sidebar
    styling is ongoing,

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,21 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <div class="grid grid-cols-6 bg-[#FAFAFA]">
-    <SideBar />
-    <main :class="`h-screen  col-start-2 col-end-6 bg-[#FAFAFA] `">
-      <div>
-        <router-view />
-      </div>
-    </main>
-  </div>
+	<div class="grid grid-cols-6 bg-[#FAFAFA]">
+	
+		<SideBar v-bind:expanded="lexpanded" />
+		<main :class="`h-screen  col-start-2 col-end-6 bg-[#FAFAFA] `">
+			<div>
+        <button @click="expand">test</button>
+				<router-view />
+			</div>
+		</main>
+	</div>
 </template>
-<script>
-import SideBar from "./components/UI/SideBar.vue";
-export default {
-  components: { SideBar },
-  data() {
-    return {
-      expandSideBar: true,
-      sideBarWidth: "",
-      rotateIcon: "",
-    };
-  },
-  methods: {
-    btoggle() {
-      this.expandSideBar = !this.expandSideBar;
-      if (this.expandSideBar) {
-        this.sideBarWidth = "";
-        this.rotateIcon = "";
-      } else {
-        this.sideBarWidth = "w-[80px]";
-        this.rotateIcon = "rotate-180";
-      }
-    },
-  },
-};
-</script>
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import { ref } from 'vue';
+import SideBar from './components/UI/SideBar.vue';
+const lexpanded = ref(true);
+function expand() {
+	lexpanded.value = !lexpanded.value;
 }
-</style>
+</script>
