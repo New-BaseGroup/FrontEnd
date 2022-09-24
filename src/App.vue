d3af3cbc11d947c248730f8a84a779007bcb6ec0
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-09-24 15:33:41 +0200

    emit toggletheme class

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
-	<div class="container flex space-x-2 h-screen w-auto">
-		<SideBar />
+  <div
+    @theme="HandleEmits"
+    class="body-color content-wrapper"
+    :class="ToggleTheme"
+  >
+    <div class="container flex space-x-2 h-screen w-full">
+      <div class="bg-background-primary content-wrapper">
+        <SideBar @theme="HandleEmits" />
+      </div>
 
-		<div class="item w-screen h-auto bg-white-700">
-			<router-view />
-		</div>
-	</div>
+      <div class="item w-screen h-auto bg-background-secondary content-wrapper">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-import {RouterView } from 'vue-router';
-import SideBar from './components/UI/SideBar.vue';
+import { ref } from "vue";
+import { RouterView } from "vue-router";
+import SideBar from "./components/UI/SideBar.vue";
+
+const ToggleTheme = ref("theme-light");
+
+function HandleEmits(data) {
+  ToggleTheme.value = data;
+  console.log("app: " + ToggleTheme.value);
+}
 </script>
