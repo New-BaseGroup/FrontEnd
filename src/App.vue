3db0fd545ef3c5505fb9970c94902f96b11457fd
Ahmad Shalabi <Ahmad.shalabi@live.com>
2022-09-28 00:54:15 +0200

    add animation to pages when rendering

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-	<div class="rootContainer" :class="store.state.toggle">
-		<SideBar />
+  <div class="rootContainer" :class="store.state.toggle">
+    <SideBar />
 
-		<div class="mainContainer">
-			<router-view />
-		</div>
-	</div>
+    <div class="mainContainer">
+      <transition
+        enter-active-class="animate__animated animate__fadeIn animate__slow"
+      >
+        <router-view />
+      </transition>
+    </div>
+  </div>
 </template>
 <script setup>
-import { RouterView } from 'vue-router';
-import SideBar from './components/UI/SideBar.vue';
-import { useStore } from 'vuex';
+import { RouterView } from "vue-router";
+import SideBar from "./components/UI/SideBar.vue";
+import "animate.css";
+import { useStore } from "vuex";
 const store = useStore();
 </script>
