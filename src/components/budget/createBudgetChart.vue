7236b715a056e3d160308f7f935883d077f2cebb
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-11-10 08:18:07 +0100

    createNewBudgetFixed

---
+++ src/components/budget/createBudgetChart.vue
@@ -0,0 +1,10 @@
+<template>
+    <DoughnutChart :chartData="testData" />
+  </template>
+  
+  <script setup>
+  import { DoughnutChart } from 'vue-chart-3';
+  import { Chart, registerables } from "chart.js";
+  Chart.register(...registerables);
+  const props = defineProps({testData: Object,})
+  </script>
